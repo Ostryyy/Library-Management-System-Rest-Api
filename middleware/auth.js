68c4bb1f49f43f6aced7add324d26e191850568e
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 14:06:29 +0200

    jwt token verification improved and nodemon added for easier development

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,16 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = (req, res, next) => {
-  const token = req.headers['authorization'];
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) return res.status(403).send('No token provided');
+
+  const token = authHeader.split(' ')[1];
   if (!token) return res.status(403).send('No token provided');
+
   jwt.verify(token, 'secret', (err, decoded) => {
-    if (err) return res.status(500).send('Failed to authenticate token');
+    if (err) {
+      return res.status(500).send('Failed to authenticate token');
+    }
     req.userId = decoded.id;
     next();
   });
