9446a9e69d468f902c719c3a2e6bae3d59c119e6
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 16:09:57 +0200

    Ensure book model returns the last inserted ID

--- models/book.js
+++ models/book.js
@@ -3,8 +3,11 @@ const db = require('../config/database');
 const Book = {
   create: (title, author, year, category, available_copies, callback) => {
     const query = `INSERT INTO books (title, author, year, category, available_copies) VALUES (?, ?, ?, ?, ?)`;
-    db.run(query, [title, author, year, category, available_copies], function(err) {
-      callback(err, this.lastID);
+    db.run(query, [title, author, year, category, available_copies], function (err) {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, this.lastID); 
     });
   },
   getAll: (callback) => {
