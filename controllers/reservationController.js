465745559068011f268d26619f0ecc90421acf6b
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 14:13:28 +0200

    Add validation to reservation controller

--- controllers/reservationController.js
+++ controllers/reservationController.js
@@ -1,13 +1,23 @@
+const { body, validationResult } = require('express-validator');
 const Reservation = require('../models/reservation');
 
-const reserveBook = (req, res) => {
-  const { bookId } = req.body;
-  const userId = req.userId;
-  Reservation.create(userId, bookId, (err, reservationId) => {
-    if (err) return res.status(500).send(err);
-    res.status(201).send({ reservationId });
-  });
-};
+const reserveBook = [
+  body('bookId').isInt({ min: 0 }).withMessage('Book ID must be a positive integer'),
+  
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    
+    const { bookId } = req.body;
+    const userId = req.userId;
+    Reservation.create(userId, bookId, (err, reservationId) => {
+      if (err) return res.status(500).send(err);
+      res.status(201).send({ reservationId });
+    });
+  }
+];
 
 const getUserReservations = (req, res) => {
   const userId = req.userId;
