7728a35823042c6e59d63fe2124feaf433b22e6e
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 16:10:01 +0200

    Add findById function to reservation model

--- models/reservation.js
+++ models/reservation.js
@@ -4,13 +4,17 @@ const Reservation = {
   create: (userId, bookId, callback) => {
     const query = `INSERT INTO reservations (user_id, book_id, reservation_date) VALUES (?, ?, ?)`;
     const date = new Date().toISOString();
-    db.run(query, [userId, bookId, date], function(err) {
+    db.run(query, [userId, bookId, date], function (err) {
       callback(err, this.lastID);
     });
   },
   getByUser: (userId, callback) => {
     const query = `SELECT * FROM reservations WHERE user_id = ?`;
     db.all(query, [userId], callback);
+  },
+  findById: (id, callback) => {
+    const query = `SELECT * FROM reservations WHERE id = ?`;
+    db.get(query, [id], callback);
   },
   delete: (id, callback) => {
     const query = `DELETE FROM reservations WHERE id = ?`;
