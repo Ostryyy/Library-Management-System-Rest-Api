7e65883e2fbd9c943d68148233c74a03f54e5a2a
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 15:13:02 +0200

    Added route to delete reservation

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { reserveBook, getUserReservations } = require('../controllers/reservationController');
+const { reserveBook, getUserReservations, cancelReservation } = require('../controllers/reservationController');
 const authMiddleware = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/', authMiddleware, reserveBook);
 router.get('/', authMiddleware, getUserReservations);
+router.delete('/:id', authMiddleware, cancelReservation);
 
 module.exports = router;
