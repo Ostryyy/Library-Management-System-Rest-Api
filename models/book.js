33f2af6287c005499f819d51dfb5a84478436bd8
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 15:07:46 +0200

    Add book search functionality

--- models/book.js
+++ models/book.js
@@ -15,6 +15,27 @@ const Book = {
     const query = `DELETE FROM books WHERE id = ?`;
     db.run(query, [id], callback);
   },
+  update: (id, updates, callback) => {
+    const query = `UPDATE books SET title = ?, author = ?, year = ?, category = ?, available_copies = ? WHERE id = ?`;
+    db.run(query, [updates.title, updates.author, updates.year, updates.category, updates.available_copies, id], callback);
+  },
+  search: (title, author, category, callback) => {
+    let query = `SELECT * FROM books WHERE 1=1`;
+    const params = [];
+    if (title) {
+      query += ` AND title LIKE ?`;
+      params.push(`%${title}%`);
+    }
+    if (author) {
+      query += ` AND author LIKE ?`;
+      params.push(`%${author}%`);
+    }
+    if (category) {
+      query += ` AND category LIKE ?`;
+      params.push(`%${category}%`);
+    }
+    db.all(query, params, callback);
+  }
 };
 
 module.exports = Book;
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { addBook, getAllBooks } = require('../controllers/bookController');
+const { addBook, getAllBooks, deleteBook, updateBook, searchBooks } = require('../controllers/bookController');
 const authMiddleware = require('../middleware/auth');
+const checkRole = require('../middleware/role');
 const router = express.Router();
 
-router.post('/', authMiddleware, addBook);
-router.get('/', getAllBooks);
+router.post('/', authMiddleware, checkRole(['admin']), addBook);
+router.get('/', authMiddleware, getAllBooks);
+router.delete('/:id', authMiddleware, checkRole(['admin']), deleteBook);
+router.put('/:id', authMiddleware, checkRole(['admin']), updateBook);
+router.get('/search', authMiddleware, searchBooks);
 
 module.exports = router;
