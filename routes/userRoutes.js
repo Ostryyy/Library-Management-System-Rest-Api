996b48564183fbe30d0969fb9a1e217e7977bc9c
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-18 15:13:14 +0200

    Added route to delete user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { register, login } = require('../controllers/userController');
+const { register, login, deleteUser, updateUser } = require('../controllers/userController');
+const authMiddleware = require('../middleware/auth');
+const checkRole = require('../middleware/role');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.delete('/:id', authMiddleware, checkRole(['admin']), deleteUser);
+router.put('/:id', authMiddleware, checkRole(['admin']), updateUser);
 
 module.exports = router;
