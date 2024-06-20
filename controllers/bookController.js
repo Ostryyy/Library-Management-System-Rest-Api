00712ed73e22ce80093cbf93321a2a7dc09c98eb
Arkadiusz Ostrowski <arek.ostrowski135@gmail.com>
2024-06-20 14:14:09 +0200

    refactor: update formatting and add getBookById function in bookController

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,12 +1,14 @@
-const { body, validationResult, param } = require('express-validator');
-const Book = require('../models/book');
+const { body, validationResult, param } = require("express-validator");
+const Book = require("../models/book");
 
 const addBook = [
-  body('title').notEmpty().withMessage('Title is required'),
-  body('author').notEmpty().withMessage('Author is required'),
-  body('year').isInt({ min: 0 }).withMessage('Year must be a positive integer'),
-  body('category').notEmpty().withMessage('Category is required'),
-  body('available_copies').isInt({ min: 0 }).withMessage('Available copies must be a positive integer'),
+  body("title").notEmpty().withMessage("Title is required"),
+  body("author").notEmpty().withMessage("Author is required"),
+  body("year").isInt({ min: 0 }).withMessage("Year must be a positive integer"),
+  body("category").notEmpty().withMessage("Category is required"),
+  body("available_copies")
+    .isInt({ min: 0 })
+    .withMessage("Available copies must be a positive integer"),
 
   (req, res) => {
     const errors = validationResult(req);
@@ -15,11 +17,18 @@ const addBook = [
     }
 
     const { title, author, year, category, available_copies } = req.body;
-    Book.create(title, author, year, category, available_copies, (err, bookId) => {
-      if (err) return res.status(500).send(err);
-      res.status(201).send({ bookId });
-    });
-  }
+    Book.create(
+      title,
+      author,
+      year,
+      category,
+      available_copies,
+      (err, bookId) => {
+        if (err) return res.status(500).send(err);
+        res.status(201).send({ bookId });
+      }
+    );
+  },
 ];
 
 const getAllBooks = (req, res) => {
@@ -29,8 +38,29 @@ const getAllBooks = (req, res) => {
   });
 };
 
+const getBookById = [
+  param("id")
+    .isInt({ min: 1 })
+    .withMessage("Book ID must be a positive integer"),
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const bookId = req.params.id;
+    Book.getById(bookId, (err, book) => {
+      if (err) return res.status(500).send(err);
+      if (!book) return res.status(404).send("Book not found");
+      res.send(book);
+    });
+  },
+];
+
 const deleteBook = [
-  param('id').isInt({ min: 1 }).withMessage('Book ID must be a positive integer'),
+  param("id")
+    .isInt({ min: 1 })
+    .withMessage("Book ID must be a positive integer"),
 
   (req, res) => {
     const errors = validationResult(req);
@@ -43,16 +73,24 @@ const deleteBook = [
       if (err) return res.status(500).send(err);
       res.status(204).send();
     });
-  }
+  },
 ];
 
 const updateBook = [
-  param('id').isInt({ min: 1 }).withMessage('Book ID must be a positive integer'),
-  body('title').optional().notEmpty().withMessage('Title is required'),
-  body('author').optional().notEmpty().withMessage('Author is required'),
-  body('year').optional().isInt({ min: 0 }).withMessage('Year must be a positive integer'),
-  body('category').optional().notEmpty().withMessage('Category is required'),
-  body('available_copies').optional().isInt({ min: 0 }).withMessage('Available copies must be a positive integer'),
+  param("id")
+    .isInt({ min: 1 })
+    .withMessage("Book ID must be a positive integer"),
+  body("title").optional().notEmpty().withMessage("Title is required"),
+  body("author").optional().notEmpty().withMessage("Author is required"),
+  body("year")
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage("Year must be a positive integer"),
+  body("category").optional().notEmpty().withMessage("Category is required"),
+  body("available_copies")
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage("Available copies must be a positive integer"),
 
   (req, res) => {
     const errors = validationResult(req);
@@ -66,7 +104,7 @@ const updateBook = [
       if (err) return res.status(500).send(err);
       res.status(200).send();
     });
-  }
+  },
 ];
 
 const searchBooks = (req, res) => {
@@ -77,4 +115,11 @@ const searchBooks = (req, res) => {
   });
 };
 
-module.exports = { addBook, getAllBooks, deleteBook, updateBook, searchBooks };
+module.exports = {
+  addBook,
+  getAllBooks,
+  getBookById,
+  deleteBook,
+  updateBook,
+  searchBooks,
+};
